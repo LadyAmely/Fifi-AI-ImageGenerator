c8c4f7b6325c5a810bbbade9d377ac72a1521583
Amelia <nawrotamelia3@gmail.com>
2025-01-01 23:05:23 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, send_from_directory, jsonify
+app = Flask(__name__, static_folder='client/build', static_url_path='/')
+@app.route('/')
+def serve_react():
+    return send_from_directory(app.static_folder, 'index.html')
+
+@app.errorhandler(404)
+def not_found(e):
+    return send_from_directory(app.static_folder, 'index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
+
+
